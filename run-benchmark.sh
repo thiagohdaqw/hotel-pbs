69e8e12e03bf6445f57b6f93904bdbf0b8c5480f
Thiago Paiva <thiagohdaqw@gmail.com>
2023-06-26 20:23:43 -0300

    adicionando benchmark compartilhado

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -8,6 +8,9 @@ export DEBUG=1
 >&2 echo "[$(date)]: $SAMPLES_FOLDER *************************************************"
 for s in $SAMPLES; do
     >&2 echo -e "$s ================================================"
-    time python3 -m hotel "$SAMPLES_FOLDER/$s" > "./result/$s";
+    timeout 600s time python3 -m hotel "$SAMPLES_FOLDER/$s" > "./result/$s";
+    if [[ "$?" != "0" ]]; then
+        >&2 echo "TLE";
+    fi;
     >&2 echo
 done
